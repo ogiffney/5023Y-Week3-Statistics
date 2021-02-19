a0c9f79feb156443e08db232a0d2b52243fdd3df
ogiffney <cbd19ktu@uea.ac.uk>
2021-02-19 13:09:17 +0000

    Finished script and added notes to each section.

--- Statistics: Regression.R
+++ Statistics: Regression.R
@@ -37,3 +37,38 @@ fitted(density_model)
 wood_density_augmented <- wood_density %>% 
   mutate(predictions=fitted(density_model)) %>% 
   mutate(residuals=Hardness-predictions)
+
+### Checking the overall explanatory power of a model
+
+library(patchwork)
+p1 <- wood_density_augmented %>% 
+  ggplot(aes(x=Density, y=Hardness))+
+  geom_line()+
+  ggtitle("Full Data")
+p2 <- wood_density_augmented %>% 
+  ggplot(aes(x=Density, y=predictions))+
+  geom_line()+
+  ggtitle("Linear trend")
+p3 <- wood_density_augmented %>% 
+  ggplot(aes(x=Density, y=residuals))+
+  geom_hline(yintercept=0, colour="white", size=5)+
+  geom_line()+
+  ggtitle("Remaining pattern")
+p1+p2+p3
+
+### Broom Package
+
+# Glance
+broom::glance(density_model)
+#Tidy
+broom::tidy(density_model, conf.int=TRUE)
+#Augment
+broom::augment(density_model, wood_density, interval="confidence") 
+
+# Plot to see confidence in the slope.
+plot1 <- broom::augment(density_model, wood_density, interval="confidence") %>% ggplot(aes(x=Density, y=Hardness))+geom_line(aes(x=Density, y=.fitted))+geom_line(aes(x=Density, y=.upper), linetype="dashed")+geom_line(aes(x=Density, y=.lower), linetype="dashed")+geom_point() +ggtitle("Manually fitting linear model \n and confidence intervals")
+plot2 <- wood_density %>% ggplot(aes(x=Density, y=Hardness))+geom_smooth(method=lm)+geom_point()+ggtitle("Geom smooth method to plotting \n a linear model")
+plot1+plot2
+
+### Write Up
+# Timber Hardness can be accurately predicted by Wood Density, as is seen in the final two graphs where the slope matches perfectly and the confidence intervals are close to the line.
